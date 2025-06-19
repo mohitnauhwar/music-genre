bc974836b9ca847716dd4d2f24b86a500156f090
mohitnauhwar <mohitnauhwar958@gmail.com>
2025-06-19 18:35:55 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 import librosa
-import tensorflow as tf
+import tflite_runtime.interpreter as tflite
 
 def extract_features(file, n_mfcc=13, duration=30):
     y, sr = librosa.load(file, duration=duration)
@@ -8,14 +8,14 @@ def extract_features(file, n_mfcc=13, duration=30):
     return np.mean(mfcc.T, axis=0).astype(np.float32).reshape(1, -1)
 
 def load_tflite_model(model_path='model_reduced.tflite'):
-    interpreter = tf.lite.Interpreter(model_path=model_path)
+    interpreter = tflite.Interpreter(model_path=model_path)
     interpreter.allocate_tensors()
     return interpreter
 
 def predict(interpreter, features):
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
-
+    
     interpreter.set_tensor(input_details[0]['index'], features)
     interpreter.invoke()
     output = interpreter.get_tensor(output_details[0]['index'])
